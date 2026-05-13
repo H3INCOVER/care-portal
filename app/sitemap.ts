313cbc9ac11427ba9b626f045b18a10cd8099a83
H3INCOVER <hira.824142@gmail.com>
2026-05-13 17:06:46 +0900

    expand sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,29 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+import facilities from "@/data/facilities.json";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://portal.h3incover.com";
+
+  const staticPages = [
+    "",
+    "/facilities",
+    "/about",
+    "/contact",
+    "/privacy",
+    "/terms",
+  ].map((path) => ({
+    url: `${baseUrl}${path}`,
+    lastModified: new Date(),
+  }));
+
+  const facilityPages = facilities.map((facility) => ({
+    url: `${baseUrl}/facilities/${facility.slug}`,
+    lastModified: new Date(),
+  }));
+
   return [
-    {
-      url: "https://portal.h3incover.com",
-      lastModified: new Date(),
-    },
+    ...staticPages,
+    ...facilityPages,
   ];
 }
