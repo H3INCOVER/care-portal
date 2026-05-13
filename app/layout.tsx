2e2901f23ca469630d5e63b00aa7e90ac47f6e45
H3INCOVER <hira.824142@gmail.com>
2026-05-13 17:42:27 +0900

    add ogp metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,52 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://portal.h3incover.com"),
+
   title: "オール介護ポータル",
 
   description:
     "地域の介護事業所を探せる介護事業所ポータルサイト",
 
+  keywords: [
+    "介護",
+    "介護事業所",
+    "訪問介護",
+    "デイサービス",
+    "福岡",
+    "介護ポータル",
+  ],
+
   icons: {
     icon: "/favicon-h3.png",
+  },
+
+  openGraph: {
+    title: "オール介護ポータル",
+    description:
+      "地域の介護事業所を探せる介護事業所ポータルサイト",
+    url: "https://portal.h3incover.com",
+    siteName: "オール介護ポータル",
+    locale: "ja_JP",
+    type: "website",
+
+    images: [
+      {
+        url: "/ogp.jpg",
+        width: 1200,
+        height: 630,
+        alt: "オール介護ポータル",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "オール介護ポータル",
+    description:
+      "地域の介護事業所を探せる介護事業所ポータルサイト",
+
+    images: ["/ogp.jpg"],
   },
 };
 
